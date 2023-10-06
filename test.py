ab3fb6e55bb614017dec36ce9ffa2f3aaf5d3663
Ankit Talele <ankit.talele76@gmail.com>
2023-10-06 14:46:33 -0400

    Added new trajectory file

--- test.py
+++ test.py
@@ -15,39 +15,34 @@ with open(csv_filepath, mode='r', newline='') as file:
     reader = csv.reader(file)
     
     # Read x coordinates
-    x_coords = [int(i) for i in next(reader)]
+    x_coords = [float(i) for i in next(reader)]
     
     # Read y coordinates
-    y_coords = [int(i) for i in next(reader)]
+    y_coords = [float(i) for i in next(reader)]
     
     # Read z coordinates
-    z_coords = [int(i) for i in next(reader)]
+    z_coords = [float(i) for i in next(reader)]
 
     # Skip the next six rows which you don't want
     for _ in range(6):
         next(reader)
 
 # Print out the lists (for testing)
-print("X Coordinates:", x_coords)
-print("Y Coordinates:", y_coords)
-print("Z Coordinates:", z_coords)
+
+x_coords = [int(x) for x in x_coords]
+y_coords = [int(y) for y in y_coords]
+z_coords = [int(z) for z in z_coords]
+
+# print("X Coordinates:", x_coords)
+# print("Y Coordinates:", y_coords)
+# print("Z Coordinates:", z_coords)
 
 tello = Tello()
 tello.connect()
 tello.takeoff()
-# tello.turn_motor_on()
-# x = 50
-# tello.move_up(x)
-# time.sleep(10)
-height = tello.get_height()
-print("Height: ", height)
-x, y, z = 50, 50, 50
-speed = 15
-tello.go_xyz_speed(x, y, z, speed)
-x, y, z = -50, -50, -50
-speed = 15
-tello.go_xyz_speed(x, y, z, speed)
-# time.sleep(10)
+speeds = 10
+for i in range(len(x_coords)):
+    tello.go_xyz_speed(x_coords[i],y_coords[i],z_coords[i],speeds)
 tello.land()
 flight_time = tello.get_flight_time()
 power = tello.get_battery()
